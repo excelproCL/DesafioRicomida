d646b0f47d9ca67053882f931b91d3f33edd025f
Sergio Cisternas <excelpro.goo@gmail.com>
2022-04-01 13:26:45 -0300

    Archivos Iniciales

---
+++ assets/js/index.js
@@ -0,0 +1,11 @@
+
+// //
+
+// const valor = prompt("Escribe un valor");
+// document.write(`<h1> ${valor} </h1>`)
+// // <h1> shd </h1>
+// console.log(valor)
+
+// document.write(`<p class='nav-bg'>  ${valor} </p>`)
+
+
